058a06b3fbaca334e6ff46f2031f85187c918cd8
Pablo Gomes <pablojr7@gmail.com>
2022-05-02 11:15:39 -0300

    chore:🚀 Stage 2 - Max aceleration

---
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -0,0 +1,79 @@
+import { FormEvent, useState } from "react";
+import { ArrowLeft } from "phosphor-react";
+import { FeedbackType, feedbackTypes } from "..";
+import { CloseButton } from "../../CloseButton";
+import { ScreenshotButton } from "../ScreenshotButton";
+
+interface FeedbackContentStepProps {
+  feedbackType: FeedbackType;
+  onFeedbackRestartRequest: () => void;
+  onFeedbackSent: () => void;
+}
+
+export function FeedbackContentStep({
+  feedbackType,
+  onFeedbackRestartRequest,
+  onFeedbackSent,
+}: FeedbackContentStepProps) {
+  const [screenshot, setScreenshot] = useState<string | null>(null);
+  const [comment, setComment] = useState("");
+
+  const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+  function handleSubmitFeedback(event: FormEvent) {
+    event.preventDefault();
+
+    console.log({
+      comment,
+      screenshot,
+    });
+
+    onFeedbackSent();
+  }
+
+  return (
+    <>
+      <header>
+        <button
+          type="button"
+          onClick={onFeedbackRestartRequest}
+          className="absolute top-5 left-5 text-zinc-400 hover:text-zinc-100"
+        >
+          <ArrowLeft weight="bold" className="w-4 h-4" />
+        </button>
+        <span className="text-xl leading-6 flex items-center gap-2">
+          <img
+            src={feedbackTypeInfo.image.source}
+            alt={feedbackTypeInfo.image.alt}
+            className="w-6 h-6"
+          />
+          {feedbackTypeInfo.title}
+        </span>
+        <CloseButton />
+      </header>
+      <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
+        <textarea
+          className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+          placeholder="Conte com detalhes o que está acontencendo..."
+          value={comment}
+          onChange={(event) => setComment(event.target.value)}
+        />
+
+        <footer className="flex gap-2 mt-2">
+          <ScreenshotButton
+            onScreenshotTook={setScreenshot}
+            screenshot={screenshot}
+          />
+
+          <button
+            type="submit"
+            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:cursor-not-allowed disabled:opacity-50 disabled:hover:bg-brand-500"
+            disabled={comment.length === 0}
+          >
+            Enviar feedback
+          </button>
+        </footer>
+      </form>
+    </>
+  );
+}
